e9b88adc3885bbdd36952db197b781b4b7ab23a4
yredjah <y.redjah@gmail.com>
2019-08-21 03:30:03 +0200

    change nav data

--- src/components/Header/data/index.js
+++ src/components/Header/data/index.js
@@ -2,35 +2,30 @@ export default [
   {
     id: '1',
     name: 'assurances',
-    active: 'assurances',
     title: 'Assurances',
     route: '/assurances',
   },
   {
     id: '2',
     name: 'placement-financier',
-    active: 'placement-financier',
     title: 'Placement financier',
     route: '/finance',
   },
   {
     id: '3',
     name: 'retraite',
-    active: 'retraite',
     title: 'Retraite',
     route: '/retraite',
   },
   {
     id: '4',
     name: 'santé-prévoyance',
-    active: 'santé-prévoyance',
     title: 'Santé / Prévoyance',
     route: '/sante_prevoyance',
   },
   {
     id: '5',
     name: 'assurance-de-prêt',
-    active: 'assurance-de-prêt',
     title: 'Assurance de prêt',
     route: '/assurance_de_pret',
   }
